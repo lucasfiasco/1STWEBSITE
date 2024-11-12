b0ce3dec4e4170cc7dca706f70bf643cdea3ea26
lucasfiasco <lucas.godek@gmail.com>
2024-11-12 20:22:13 +0000

    Newest version

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-
 document.querySelectorAll('.box').forEach(box => {
     box.addEventListener('click', () => {
         box.classList.toggle('active');
+    });
+});
+
+document.querySelectorAll('.toggle-button').forEach(button => {
+    button.addEventListener('click', () => {
+        const content = button.nextElementSibling;
+        
+        if (content) {
+            content.style.display = content.style.display === 'block' ? 'none' : 'block';
+        }
     });
 });
